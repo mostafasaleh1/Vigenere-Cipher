90a1b2f39ba2878ff23f6441f58cbe349c2556d3
Mostafa Saleh <mostafasaleh2055@gmail.com>
2023-05-22 04:45:54 +0300

    adding vigenere cipher files

---
+++ Vigenere project VS 2019/Caesar/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Caesar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Elephant", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(778, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(425, 103);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Vigenere";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(835, 325);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(279, 53);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cooper Black", 21.75F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(928, 267);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 42);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Key";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cooper Black", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(141, 206);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(487, 50);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Text to be Encrypted";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.Label3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F);
+            this.textBox2.Location = new System.Drawing.Point(16, 258);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(798, 595);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cooper Black", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(1352, 206);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(357, 50);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Encrypted text";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.Label4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(255)))), ((int)(((byte)(200)))));
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(1137, 258);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(807, 595);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextChanged += new System.EventHandler(this.TextBox3_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Cooper Black", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(823, 524);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(307, 96);
+            this.button1.TabIndex = 7;
+            this.button1.Text = " Encrypt 🢂";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.Items.AddRange(new object[] {
+            "I want to Encrypt text with Vigenere",
+            "I want to Decrypt text with Vigenere",
+            "None"});
+            this.comboBox1.Location = new System.Drawing.Point(727, 122);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(508, 33);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Cooper Black", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(822, 524);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(307, 96);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "🡸 Decrypt ";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Cooper Black", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.button3.Location = new System.Drawing.Point(823, 758);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(307, 96);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Clear All";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1924, 881);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Cassar, Dr. Osama Tharwat, Mostafa Saleh7, Sec 2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        public System.Windows.Forms.Button button1;
+        public System.Windows.Forms.Button button2;
+        public System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Vigenere project VS 2019/Caesar/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Caesar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Visible = false;
+            textBox2.Visible = false;
+            textBox3.Visible = false;
+            label4.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            button1.Visible = false;
+            button2.Visible = false;
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            /**
+             * Behaviour elements according to the choice of the combobox
+             */
+
+            /* making elements appear when a choice made in the text box */
+            textBox1.Visible = true;
+            textBox2.Visible = true;
+            textBox3.Visible = true;
+            label4.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            /* clearing all text in text boxes when a choice made in the combo box*/
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+
+            if (comboBox1.Text == "I want to Encrypt text with Vigenere") // When this choice is chosen, button 1 will appear, button 2 will disappear, and text on labels 3 and 4 will be changed as shown
+            {
+
+                button1.Visible = true;
+                button2.Visible = false;
+                label3.Text = "Text to be Encrypted";
+                label4.Text = "Encrypted Text";
+
+            }
+            else if(comboBox1.Text == "I want to Decrypt text with Vigenere") // When this choice is chosen, button 1 will disappear, button 2 will appear, and text on labels 3 and 4 will be changed as shown
+            {
+
+                button2.Visible = true;
+                button1.Visible = false;
+                label3.Text = "Decrypted Text";
+                label4.Text = "Text to be Decrypted";
+            }
+            else if (comboBox1.Text == "None") // When this choice is chosen, all elements will disappear again
+            {
+                textBox1.Visible = false;
+                textBox2.Visible = false;
+                textBox3.Visible = false;
+                label4.Visible = false;
+                label2.Visible = false;
+                label3.Visible = false;
+                button1.Visible = false;
+                button2.Visible = false;
+            }
+            else // Print error message if the user entered a strange choice
+            {
+                MessageBox.Show("Error: Please choose from the selected options");
+            }
+        }
+
+        private void Label5_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(textBox1.Text != "" || textBox1.Text == null) // if the user entered a key without errors do the following code
+                {
+                    string key = textBox1.Text; // convert the key into an integer
+                            for (int i = 0; ; i++)
+                            {
+                                if (textBox2.Text.Length == i)
+                                {
+                                    i = 0;
+                                }
+                                if (key.Length == textBox2.Text.Length)
+                                {
+                                    break;
+                                }
+                                key += (key[i]);
+                            }
+                            string EncryptedText = "";// declare  empty string.
+                                                      /* Iterate through the input text to be encrypted and substitute into the following equations everytime to get the equivalent encrypted letters*/
+                            for (int i = 0; i < textBox2.Text.Length; i++)
+                            {
+                                if (char.IsLetter(textBox2.Text[i])) // if input text is consisted of letters, do the following.
+                                {
+                                    if (char.IsLower(textBox2.Text[i])) // if the input text is lower case, do the following.
+                                    {
+                                        EncryptedText += (char)((((textBox2.Text[i] - 32) + key[i]) % 26) + 65); // encryption equation for small letters + concatinate each encrypted (shifted) letter to the variable EncryptedText
+                                    }
+                                    else // if the letters aren't small, then it's capital, and do the following
+                                    {
+                                        EncryptedText += (char)(((textBox2.Text[i] + key[i]) % 26) + 65);// encryption equation for capital letters + concatinate each encrypted (shifted) letter to the variable EncryptedText
+                                    }
+                                }
+                                else // if input text is not consisted of letters, do the following.
+                                {
+                                    EncryptedText += textBox2.Text[i]; // concatinate each non-letter symbol as it is to the resulting text
+                                }
+                            }
+                            textBox3.Text = EncryptedText; // put the final result in the text box 3
+                }
+                else // if the user didn't enter a key print this message box
+                {
+                    MessageBox.Show("Error: You must enter a key");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text != "" || textBox1.Text == null) // if the user entered a key without errors do the following code
+                {
+                    string key = textBox1.Text;
+                            for (int i = 0; ; i++)
+                            {
+                                if (textBox3.Text.Length == i)
+                                {
+                                    i = 0;
+                                }
+                                if (key.Length == textBox3.Text.Length)
+                                {
+                                    break;
+                                }
+                                key += (key[i]);
+                            }
+                            string DecryptedText = ""; // declare an empty string.
+                                                       /* Iterate through the input text to be Decrypted and substitute into the following equations everytime to get the equivalent Decrypted letters*/
+                            for (int i = 0; i < textBox3.Text.Length && i < key.Length; i++)
+                            {
+                                if (char.IsLetter(textBox3.Text[i])) // if input text is consisted of letters, do the following.
+                                {
+                                    if (char.IsLower(textBox3.Text[i])) // if the input text is lower case, do the following.
+                                    {
+                                        DecryptedText += (char)((((textBox3.Text[i] - 32) - key[i] + 26) % 26) + 65); // Decryption equation for small letters + concatinate each Decrypted (shifted) letter to the variable EncryptedText
+                                    }
+                                    else // if the letters aren't small, then it's capital, and do the following
+                                    {
+                                        DecryptedText += (char)(((textBox3.Text[i] - key[i] + 26) % 26) + 65); // Decryption equation for capital letters + concatinate each Decrypted (shifted) letter to the variable EncryptedText
+                                    }
+                                }
+                                else // if input text is not consisted of letters, do the following.
+                                {
+                                    DecryptedText += textBox3.Text[i]; // concatinate each non-letter symbol as it is to the resulting text
+                                }
+                            }
+                            textBox2.Text = DecryptedText; // put the final result in the text box 2
+                }
+                else // if the user didn't enter a key print this message box
+                {
+                    MessageBox.Show("Error: You must enter a key");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+    }
+}
